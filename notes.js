490ae42afa2e60e53e04739394bac1dc5e40aee7
Marcelo Gonzalez <mfgonzalez@gmail.com>
2018-05-18 05:17:25 -0300

    More files from training

--- notes.js
+++ notes.js
@@ -2,23 +2,31 @@ console.log('Starting notes.js...');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (error) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     }
 
-    try {
-        var notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (error) {
-    }
-
     var duplicatedNotes = notes.filter((note) => note.title === title);
     if (duplicatedNotes.length === 0) {
         notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+        saveNotes(notes);
+        return note;
     }
 
 };
@@ -28,16 +36,29 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getting note', title);
+    var notes = fetchNotes();
+    var filteredNote = notes.filter((note) => note.title === title);
+    return filteredNote[0];
 };
 
 var removeNote = (title) => {
-    console.log('Removing note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
 };
+
+var logNote = (note) => {
+    console.log('---');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+};
+
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
---
+++ playground/debbuging.js
@@ -0,0 +1,9 @@
+var person = {
+    name: 'Marcelo'
+};
+
+person.age = 40;
+
+person.name = 'Miguel';
+
+console.log(person);
